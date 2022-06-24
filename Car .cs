043e6656d7ce5e8e10d747c61d2b270f58f7c79e
Vencislav <vencislav67@hotmail.com>
2022-06-24 13:44:08 +0300

    Add project files.

---
+++ Car .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    internal class Car
+    {
+        private string make;
+        private string model;
+        private int horsePower;
+        private string regNumber;
+
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public string RegNumber { get; set; }
+
+    }
+}
